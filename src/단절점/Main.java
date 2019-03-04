ce86391a7a1bd48e20d0f9cc9235e3b6ece5d0f6
lipnus <sunpil13@korea.ac.kr>
2019-03-04 17:34:57 +0900

    add 단절점

--- src/단절점/Main.java
+++ src/단절점/Main.java
@@ -11,11 +11,11 @@ public class Main {
     static int V; //정점의 수 ~10,000
     static int E; //간선의 수 ~100,000
     static List<Integer>[] map;
-    static int[] cutPoint;
+    static int[] cutVertex; //단절점
 
 
-    static int[] order;
-    static int seq; //방문순서
+    static int[] order; //각 정점의 방문순서
+    static int seq; //전체 방문순서 카운터
 
 
     public static void main(String[] args) throws Exception{
@@ -26,9 +26,9 @@ public class Main {
         V = Integer.parseInt(st.nextToken());
         E = Integer.parseInt(st.nextToken());
         order = new int[V+1]; //0이면 방문을 하지 않은 상태
+        cutVertex = new int[V+1];
 
-        seq = 0;
-        cutPoint = new int[V+1];
+        seq = 1;
 
         //몇개 안되니까 초기화를 다 시킨다
         map = new ArrayList[V+1];
@@ -46,20 +46,19 @@ public class Main {
 
         //forest가 2개 이상일 수 있으므로.
         for(int i=1; i<=V; i++){
-            if(order[i]==0) dfs(true, i);
+            if(order[i]==0) dfs(i, true);
         }
 
-
+        //출력
         int cutPointCount = 0;
         for(int i=1; i<=V; i++){
-            if(cutPoint[i]==1) cutPointCount++;
+            if(cutVertex[i]==1) cutPointCount++;
         }
+
         System.out.println(cutPointCount);
 
         for(int i=1; i<=V; i++){
-            if(cutPoint[i]==1){
-                System.out.printf("%d ", i);
-            }
+            if(cutVertex[i]==1) System.out.printf("%d ", i);
         }
     }
 
@@ -68,28 +67,36 @@ public class Main {
      * 어떤(now)의 다음 노드(next)가  now노드를 거치지 않고 그 앞의 애들에게 접근할 수 없으면 단절점.
      * 리턴값은 탐색가능한 노드 중 seq가 가장 작은 노드의 seq.
      *
-     * [예외] Root의 경우는 자손이 2개 이상이면 단절점.
+     * [예외] root의 경우는 자손이 2개 이상이면 단절점.
      */
-    static int dfs(Boolean isRoot, int now){
+    static int dfs(int now, boolean isRoot){
 
-        order[now] = ++seq; //현재노드의 방문번호
-        int returnValue=order[now];
-
-        //루트 예외처리
-        if(isRoot && map[now].size() >=2) cutPoint[now]=1;
+        order[now] = seq++; //현재노드의 방문번호
+        int retu = order[now]; //가장 말단이면 자기 자신의 order를 반환
+        int childCount=0;
 
 
+        //다음노드 검사
         for(int next: map[now]){
 
-            if(order[next]==0){ //갈 수 있는 곳
+            if(order[next]==0){ //새로운 곳(탐색O)
 
-                int dfs = dfs(false, next);
-                if( order[now] <= dfs && isRoot==false) cutPoint[now]=1; //다음노드가 나 이전으로 가지 못하면 나는 단절점
+                childCount++;
+                int dfs = dfs(next, false);
+                if( order[now]<=dfs && !isRoot ){
+                    System.out.printf("단절점추가:%d(%d) %d(%d)\n", now, order[now], next, dfs);
+                    cutVertex[now]=1; //다음노드가 나 이전으로 가지 못하면 나는 단절점
+                }
+                retu = Math.min(retu, dfs);
 
-            } else { //이미 간 곳은 order만 가져오고 탐색은 하지 않음
-                returnValue = Math.min(returnValue, order[next]);
+            }else { //이미 갔던 곳: order만 가져온다 (탐색X)
+                retu = Math.min(retu, order[next]);
             }
         }
-        return returnValue;
+
+        //루트이면서 연결지점이 2개이상인 경우 단절점
+        if( isRoot && 2<=map[now].size() ) cutVertex[now]=1;
+
+        return retu;
     }
 }
